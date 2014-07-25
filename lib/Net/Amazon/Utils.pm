a58190d6d30d35709f90b1b91d538d97b6112ccd
Gonzalo Barco <gbarcouy@gmail.com>
2014-07-25 12:23:17 -0300

    Initial commit of Net::Amazon::Utils.

---
+++ lib/Net/Amazon/Utils.pm
@@ -0,0 +1,107 @@
+package Net::Amazon::Utils;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Net::Amazon::Utils - The great new Net::Amazon::Utils!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Amazon::Utils;
+
+    my $foo = Net::Amazon::Utils->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Gonzalo Barco, C<< <gbarco uy at gmail com, no spaces> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-amazon-utils at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Amazon-Utils>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Amazon::Utils
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Amazon-Utils>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-Amazon-Utils>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net-Amazon-Utils>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Net-Amazon-Utils/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Gonzalo Barco.
+
+This program is released under the following license: gpl, artistic
+
+
+=cut
+
+1; # End of Net::Amazon::Utils
