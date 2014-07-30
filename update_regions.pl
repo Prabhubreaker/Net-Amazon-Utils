6bd9873aa957ac7d6ab5931f0280b30287bd788c
Gonzalo Barco <gbarcouy@gmail.com>
2014-07-30 02:50:17 -0300

    Add POD coverage to Net::Amazon::Utils::Regions generator.

--- update_regions.pl
+++ update_regions.pl
@@ -9,20 +9,34 @@ my $utils = Net::Amazon::Utils->new(0,0);
 
 my $uri = $utils->_get_remote_regions_file_uri();
 
-my $regions = $utils->_get_regions_file_raw();
-
 print STDERR "Will download $uri\n";
+
+my $regions = $utils->_get_regions_file_raw();
 
 print STDERR "Will try to write updated class file.\n";
 
 mkdir 'lib/Net/Amazon/Utils';
 
-open ( LIB, '>lib/Net/Amazon/Utils/Regions.pm' ) || die("Could not open Regions module for writing.");
+open ( LIB, '>lib/Net/Amazon/Utils/Regions.pm' ) || die("Could not open Regions module for writing." );
 
-LIB->print( "package Net::Amazon::Utils::Regions;\n\n");
-LIB->print( "use strict;\n\n");
+LIB->print( "package Net::Amazon::Utils::Regions;\n\n" );
+LIB->print( "use strict;\n\n" );
 
-LIB->print( "# Generated from L<$uri>\n\n");
+LIB->print( "=head1 NAME\n\n" );
+
+LIB->print( "Net::Amazon::Utils::Regions - Data for Net::Amazon::Utils::Regions." );
+
+LIB->print( "# Generated from L<$uri>\n\n" );
+
+LIB->print( "=head1 VERSION\n\n" );
+
+LIB->print( "Version 0.01\n\n" );
+
+LIB->print( "=head2 get_regions_datan\n\n" );
+
+LIB->print( "Returns the structure of regions.xml.\n\n" );
+
+LIB->print( "=cut\n\n" );
 
 $Data::Dumper::Indent = 1;
 $Data::Dumper::Terse = 1;
